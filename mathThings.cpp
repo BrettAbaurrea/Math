5957b5a09679d70ed980c03d863cc0448d150b5f
Brett Abaurrea <91746166+BrettAbaurrea@users.noreply.github.com>
2024-02-03 00:07:27 -0500

    Basic Coin Toss
    I want to see if the percentage of heads/tails is really 50%. This initial math formula is just a coin toss

---
+++ mathThings.cpp
@@ -0,0 +1,40 @@
+/*
+*   Random math things I need
+*/
+
+#include <iostream>
+#include <stdlib.h> //rand
+
+int main()
+{
+    //coin flip
+    srand(time(NULL));
+    int tossTracker = 0;
+    int flipAmt = 0;
+    int numHeads = 0;
+    int numTails = 0;
+
+    std::cout << "How many flips? ";
+    std::cin >> flipAmt;
+    std::cout << "Tossing.....\t bitch\n";
+    do{
+        int flip = (rand()%2);
+        std::cout << flip;
+        tossTracker++;
+        if (flip == 0){
+            numHeads++;
+        }
+        else if (flip == 1){
+            numTails++;
+        }
+        else
+            return 0;
+    }while (tossTracker <= flipAmt);
+
+    std::cout << "\nHeads: " << numHeads << "\n";
+    std::cout << "Tails: " << numTails << "\n";
+
+
+
+    return 0;
+}
